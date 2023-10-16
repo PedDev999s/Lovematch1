e610c6ad77c9e9555893c0d2a580981895241e3e
Pedro Amorim <pedroamorimdev@gmail.com>
2023-10-16 17:30:15 -0300

    Adicionar arquivos de projeto.

---
+++ Lovematch1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lovematch1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
